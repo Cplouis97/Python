23080648dc97f7d308f076afe326b5d425612b50
Carl <cplouis97@gmail.com>
2019-09-29 18:06:12 -0400

    Final changes for reverse string app

--- Practice.py
+++ Practice.py
@@ -1,2 +1,26 @@
-print("Hello World")
+def reverse(x):
+    sLength = len(x)
+    placeholder = list(x)
+    sLength = sLength -1
+    i = 0
+    #placeholder = list(placeholder)
+
+    while sLength > - 1:
+        placeholder[i] = x[sLength]
+
+        i = i +1
+        sLength = sLength - 1
+    rStr = "".join(placeholder)
+    return rStr
+
+
+
+
+
+print("----------Reverse String APP----------")
+y = input("Enter A String: ")
+
+fStr = reverse(y)
+
+print("Your original String was: " + y + "\nYour reversed string is: "  + fStr)
 
