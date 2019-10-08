194b6110b9b30827a81ea57bdd1d452efce131de
Carl <cplouis97@gmail.com>
2019-10-08 12:35:52 -0400

    Coding Challenge 1

--- CodingChallenge1.py
+++ CodingChallenge1.py
@@ -2,29 +2,29 @@ def main():
     j = 1
     w = 1
     fAns = False
-    nums = input("Enter Values: ")
-    nums = nums.split(" ")
-    for l in range(len(nums)):
+    nums = input("Enter Values Separated by a space: ") # user enters values
+    nums = nums.split(" ") #Split here to remove the spaces from the users entry
+
+    for l in range(len(nums)): #Each value in the array is then converted to an integer value
         nums[l] = int(nums[l])
-    val1 = input("Enter Value: ")
-    val2 = len(nums)
-    print(nums)
-    for i in range(val2):
+    val1 = input("Enter Value: ") #Here the user enters a number which we are to find two values in the array that add up to that value
+    val2 = len(nums) #length of the array is calculated to later be used within the for loop
+    for i in range(val2): #loop to cycle through values within array
         j = w
         # print("Top j: " + str(j))
-        for k in range(val2):
-            if j == val2:
+        for k in range(val2): # Another loop to add numbers in the array together
+            if j == val2: #This will act as an error handler so the array does not go out of bounds
                 break
             sum1 = nums[i] + nums[j]
-            if sum1 == int(val1):
+            if sum1 == int(val1): #if we find two numbers that satifies the answer the boolean value becomes true
                 # print("Answer found")
                 fAns = True
                 break
-            j += 1
+            j += 1 # the value j is incremented to cycle through array
         if fAns:
             break
         w += 1
-    aIndex = [i, j]
+    aIndex = [i, j] #Indicies of values that add up to the given number
     if fAns:
         print(aIndex)
     else:
